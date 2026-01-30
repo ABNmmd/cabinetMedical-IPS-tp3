b8bfd37ddb63a0b5be90eced5788506d05892887
ABNmmd <moha.ww.ha@gmail.com>
2026-01-30 01:42:17 +0100

    update IDs in data.sql for consultations, doctors, patients, and appointments

--- patient-service/src/main/resources/data.sql
+++ patient-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (1, 'Ahmed Amine', '1990-05-12', 'M', '0612345678');
-INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (2, 'Sara El Idrissi', '1995-11-03', 'F', '0622334455');
-INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (3, 'Youssef Karim', '1988-02-24', 'M', '0600112233');
+INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (15, 'Ahmed Amine', '1990-05-12', 'M', '0612345678');
+INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (23, 'Sara El Idrissi', '1995-11-03', 'F', '0622334455');
+INSERT INTO patient (id, nom, date_naissance, genre, telephone) VALUES (32, 'Youssef Karim', '1988-02-24', 'M', '0600112233');
 
