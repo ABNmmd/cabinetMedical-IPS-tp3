6fa6afefebf0e71c5e035299ee0f885894f5565f
ABNmmd <moha.ww.ha@gmail.com>
2026-02-01 19:49:11 +0100

    Update test data dates in consultation and rendezvous services

--- consultation-service/src/main/resources/data.sql
+++ consultation-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO consultation (id, date_consultation, rapport, rendez_vous_id) VALUES (19, '2026-02-16', 'Consultation initiale, patient stable.', 1);
-INSERT INTO consultation (id, date_consultation, rapport, rendez_vous_id) VALUES (29, '2026-02-21', 'Suivi dermatologique, traitement adapte.', 2);
+INSERT INTO consultation (id, date_consultation, rapport, rendez_vous_id) VALUES (19, '2027-03-16', 'Consultation initiale, patient stable.', 1);
+INSERT INTO consultation (id, date_consultation, rapport, rendez_vous_id) VALUES (29, '2027-03-21', 'Suivi dermatologique, traitement adapte.', 2);
 
